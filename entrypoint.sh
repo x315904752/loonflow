476698b0aed0cb7abe8dd14711dcebcbf9eca967
徐磊 <x@iparp.com>
2020-02-22 09:58:26 +0800

    完善docker化

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+touch /opt/loonflow.sock && touch /opt/loonflow.pid && touch /var/log/loonflow.log
 python /opt/loonflow/manage.py migrate
 uwsgi --ini /opt/loonflow/uwsgi.ini
 tail -f /var/log/loonflow.log
