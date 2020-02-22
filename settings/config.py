470ce01949ac1be136dbe1185fd2930818e7dc07
徐磊 <x@iparp.com>
2020-02-22 10:52:50 +0800

    设置从环境变量中捕获配置信息

--- settings/config.py
+++ settings/config.py
@@ -8,13 +8,25 @@ config_url = os.path.join(os.path.split(cur_path)[0], 'config.ini')
 conf.read(config_url)
 
 
-MYSQL_HOST = conf.get('mysql', 'MYSQL_HOST')
-MYSQL_NAME = conf.get('mysql', 'MYSQL_NAME')
-MYSQL_PORT = conf.get('mysql', 'MYSQL_PORT')
-MYSQL_USER = conf.get('mysql', 'MYSQL_USER')
-MYSQL_PASSWORD = conf.get('mysql', 'MYSQL_PASSWORD')
+def getvalue_from_env(env_name, section, option, default_value):
+    value = os.environ.get(env_name, None)
+    if value:
+        return value
+    else:
+        try:
+            return conf.get(section, option)
+        except (configparser.NoOptionError, configparser.NoSectionError):
+            return default_value
 
-REDIS_HOST = conf.get('redis', 'REDIS_HOST')
-REDIS_PORT = conf.get('redis', 'REDIS_PORT')
-REDIS_DB = conf.get('redis', 'REDIS_DB')
-REDIS_PASSWORD = conf.get('redis', 'REDIS_PASSWORD')
+
+MYSQL_HOST = getvalue_from_env('MYSQL_HOST', 'mysql', 'MYSQL_HOST', '127.0.0.1')
+MYSQL_NAME = getvalue_from_env('MYSQL_NAME', 'mysql', 'MYSQL_NAME', 'loonflow')
+MYSQL_PORT = getvalue_from_env('MYSQL_PORT', 'mysql', 'MYSQL_PORT', 3306)
+MYSQL_USER = getvalue_from_env('MYSQL_USER', 'mysql', 'MYSQL_USER', 'root')
+MYSQL_PASSWORD = getvalue_from_env('MYSQL_PASSWORD', 'mysql', 'MYSQL_PASSWORD', '')
+
+REDIS_HOST = getvalue_from_env('REDIS_HOST', 'redis', 'REDIS_HOST', '127.0.0.1')
+REDIS_PORT = getvalue_from_env('REDIS_PORT', 'redis', 'REDIS_PORT', 6379)
+REDIS_DB = getvalue_from_env('REDIS_DB', 'redis', 'REDIS_DB', 0)
+REDIS_PASSWORD = getvalue_from_env('REDIS_PASSWORD', 'redis', 'REDIS_PASSWORD', '')
+
