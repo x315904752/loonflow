3816fc2fb66b7474a804bbb06aa6ebf0afe8be98
徐磊 <x@iparp.com>
2020-02-22 13:30:08 +0800

    设置环境变量

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-/usr/sbin/groupadd loonflow
-/usr/sbin/useradd -g loonflow loonflow
 touch /opt/loonflow.sock && touch /opt/loonflow.pid && touch /var/log/loonflow.log
 python /opt/loonflow/manage.py migrate
 uwsgi --ini /opt/loonflow/uwsgi.ini
